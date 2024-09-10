9a2d803a6189d0b5b1d88911da426a453d3ee00b
Amanmyrat <amansarahs@gmail.com>
2024-09-10 11:04:28 +0500

    Added buildspec file

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://localhost:8000/',
+    baseUrl: 'http://backend.babajangurlusyk.transfertakip.com/',
     apiV1: 'api/v1/',
     // Other development-specific variables (e.g., logging levels)
     buildTimestamp: new Date().toISOString(),
